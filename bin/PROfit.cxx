925d27b8b282cb6f684163165e4d6404169df52f
markrosslonergan <markrosslonergan@gmail.com>
2023-03-09 23:04:57 -0700

    PROfit bin test

---
+++ bin/PROfit.cxx
@@ -0,0 +1,29 @@
+#include "PROconfig.h"
+#include "PROspec.h"
+
+#include "CLI11.h"
+
+using namespace PROfit;
+
+int main(int argc, char* argv[])
+{
+
+    CLI::App app{"Test for PROfit"}; 
+
+    // Define options
+    std::string xmlname = "NULL.xml"; 
+    int maxevents = 100;
+
+    //doubles
+    app.add_option("-x,--xml", xmlname, "Input PROfit XML config.");
+    app.add_option("-m,--max", maxevents, "Max number of events to run over.");
+
+    CLI11_PARSE(app, argc, argv);
+
+
+
+
+
+    return 0;
+}
+
