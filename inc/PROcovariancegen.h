a55c5175b15ffbdd0a08810b2e2f70ac2fe5fa4f
markrosslonergan <markrosslonergan@gmail.com>
2023-04-03 12:16:18 -0400

    actually added PROcovariancegen

---
+++ inc/PROcovariancegen.h
@@ -0,0 +1,69 @@
+#ifndef PROCOVARIANCEGEN_H_
+#define PROCOVARIANCEGEN_H_
+
+// STANDARD
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <ctime>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+
+// ROOT
+#include "TH1D.h"
+
+// EIGEN
+#include <Eigen/Dense>
+#include <Eigen/SVD>
+
+// PROfit
+#include "PROconfig.h"
+
+//Delete when ready
+#include "TMatrixDEigen.h"
+#include "TMatrixDSymEigen.h"
+#include "TMatrixD.h"
+#include "TMatrixT.h"
+#include "TDirectory.h"
+#include "TVectorD.h"
+#include "TRandom3.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TH2D.h"
+#include "TNtuple.h"
+#include "TLine.h"
+
+#include "TText.h"
+#include "TROOT.h"
+#include "TRint.h"
+#include "TGraph.h"
+#include "TSystem.h"
+#include "TStyle.h"
+#include "TLorentzVector.h"
+#include "TVector3.h"
+#include "THnSparse.h"
+#include "TStopwatch.h"
+#include "TTreeFormula.h"
+#include "TCanvas.h"
+
+#include <sys/stat.h> 
+
+//#define TYPE_FLOAT
+#ifdef TYPE_FLOAT  
+    typedef float eweight_type;
+#else
+    typedef double eweight_type;
+#endif
+
+
+namespace PROfit{
+
+
+int generateFracCovarianceFromXML(const PROconfig &inconfig, Eigen::MatrixXd &out_frac_covar);
+
+
+};
+#endif
