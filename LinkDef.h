f9a4e7947bec5f7da58e4a78f151edc29c38fc74
Jacob Larkin <jacob.larkin@stonybrook.edu>
2023-04-04 15:20:26 -0500

    Generate dictionary with LinkDef

---
+++ LinkDef.h
@@ -0,0 +1,7 @@
+#ifdef __CINT__
+//#pragma link C++ namespace caf;
+#pragma link C++ class caf::SRGlobal+;
+#pragma link C++ class caf::SRWeightPSet+;
+#pragma link C++ class caf::SRWeightMapEntry+;
+#pragma link C++ class caf::SRWeightParam+;
+#endif
