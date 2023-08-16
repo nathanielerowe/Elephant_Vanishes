25a6d05e8b7cb7ff5b3a69341dd48232064c93f3
Ibrahim Safa <isafa@sbndgpvm02.fnal.gov>
2023-08-16 15:43:03 -0500

    switched to highfive. added PROtransformer, a script that transforms root into h5 file

--- bin/PROtransformer.cxx
+++ bin/PROtransformer.cxx
@@ -13,7 +13,7 @@
 #include <memory>
 #include <string>
 #include <cstdlib>
-#include <hdf5.h>
+#include <highfive/H5File.hpp>
 
 using namespace PROfit;
 log_level_t GLOBAL_LEVEL = LOG_DEBUG;
@@ -22,18 +22,16 @@ int main(int argc, char* argv[]){
  
   CLI::App app{"Test for PROfit"};
   // Define options
-  std::string filename = "NULL.flat.caf.root";
-  std::string loadtype = "NONE";
-  app.add_option("-f,--file", filename, "File to read systematics from.");
-  app.add_option("-t,--type", loadtype, "h5 or root?");
+  std::string infilename = "NULL.flat.caf.root";
+  std::string outfilename = "null.h5";
+  app.add_option("-o,--outfile", outfilename, "H5 file to output to.");
+  app.add_option("-i, --infile", infilename, "root file input");
   app.add_option("-v,--verbosity", GLOBAL_LEVEL, "Verbosity Level [1-4].");
  
   CLI11_PARSE(app, argc, argv);
 
-
-  if(loadtype=="root"){
   //open flat caf file
-  auto flat_caf = std::make_unique<TFile>(filename.c_str(),"read");
+  auto flat_caf = std::make_unique<TFile>(infilename.c_str(),"read");
   std::cout<< "opened file!" << std::endl;
   TTree *mytree = (TTree*)flat_caf->Get("nutree");
   std::cout<< "defined tree!"<<std::endl;
@@ -46,69 +44,29 @@ int main(int argc, char* argv[]){
   mytree->SetBranchAddress("baseline", &baseline);
 
   std::cout << "set branch addresses" << std::endl;
+  //std::vector<float> v_trueE;
+  std::vector<float> v_recoE;
+  //std::vector<float> v_baseline;
+  std::vector<float> L_over_E;
 
   for(int i = 0 ; i != mytree->GetEntries(); ++i){
       mytree->GetEntry(i);
+      //v_trueE.push_back(trueE);
+      v_recoE.push_back(recoE);
+      //v_baseline.push_back(baseline);
+      L_over_E.push_back(baseline/recoE);
   }
-  }
+  //}
 
-  else if(loadtype=="h5"){
+  HighFive::File myfile(outfilename, HighFive::File::Truncate);
+  //myfile.createDataSet("nugroup/trueE", trueE);
+  myfile.createDataSet("nugroup/recoE", v_recoE);
+  //myfile.createDataSet("nugroup/baseline", baseline);
+  myfile.createDataSet("nugroup/LE", L_over_E);
+  /*auto dset1 = myfile.getDataSet("/nugroup/trueE/")
+  auto trueE = dset1.read<std::vector<float>>
 
-    hid_t file_id, dset_id, dspace_id, group_id; /* identifiers */
-    herr_t status;
-    int ndims = 1;
-    const char* groupName = "nugroup"; // Group containing the dataset
-    const char* datasetName = "trueE";
-
-    file_id = H5Fopen(filename.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
-    std::cout<< "opening file!" << std::endl;
-    //hid_t group_id = H5Gopen(file_id, groupName, H5P_DEFAULT);
-    //std::cout<< "opening group!" << std::endl;
-    //hid_t dataset_id = H5Dopen(group_id, datasetName, H5P_DEFAULT);
-    //hid_t dataspace_id = H5Dget_space(dataset_id);
-    //int num_dimensions = H5Sget_simple_extent_ndims(dataspace_id);
-    //hsize_t dimensions[num_dimensions];
-    //H5Sget_simple_extent_dims(dataspace_id, dimensions, NULL);
-    //size_t data_size = H5Tget_size(H5Dget_type(dataset_id));
-    //void* data_buffer = malloc(data_size);
-    //std::cout<< "opening dataset!" << std::endl;
-    
-
-    //Retrieve result size and preallocate vector
-    std::vector<double> result;
-    dset_id = H5Dopen(file_id, "/nugroup/trueE", H5P_DEFAULT);
-    dspace_id = H5Dget_space(dset_id);
-    ndims = H5Sget_simple_extent_ndims(dspace_id);
-    hsize_t res_dims[ndims];
-    std::cout<<"size is : "<<ndims<< std::endl;
-    status = H5Sget_simple_extent_dims(dspace_id, res_dims, NULL);
-    int res_sz = 1;
-    for (int i = 0; i < ndims; i++) {
-      res_sz *= res_dims[i];
-                                    }
-    result.resize(res_sz);
-    std::cout<<res_sz<<" this is supposedly the length of result"<<std::endl; 
-    // Read the data
-    status = H5Dread(dset_id, H5T_IEEE_F64LE, H5S_ALL, H5S_ALL, H5P_DEFAULT, result.data());
-    std::cout<<"size of input: "<<result.size()<<"status is: "<<status<<std::endl;
-    for(int i=0; i < result.size(); i++){
-        result.at(i);
-                                        }
-    //H5Dread(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, data_buffer);
-    //for (hsize_t i = 0; i < dimensions[0]; ++i) {
-    //    hsize_t start[] = {i, 0, 0};
-    //    H5Sselect_hyperslab(dataspace_id, H5S_SELECT_SET, start, NULL, dimensions, NULL);
-    //    H5Dread(dataset_id, H5T_NATIVE_CHAR, H5S_ALL, dataspace_id, H5P_DEFAULT, data_buffer);
-    //    printf("Entry %llu: %s\n", (unsigned long long)i, (char*)data_buffer);
-    //}
-
-    //free(data_buffer);
-    //H5Sclose(dataspace_id);
-    //H5Dclose(dataset_id);
-    //H5Gclose(group_id);
-    H5Fclose(file_id);
-
-  }
+  //}
   //PROcess_WeightMap(flat_filename, selection_filename, destination_file);
   
     return 0;
