41f568d39a5bd3b23e94f4b29f4d16eeec62a8b2
Ibrahim Safa <isafa@sbndgpvm04.fnal.gov>
2023-08-17 17:36:24 -0500

    fixed executable and removed unused variable error for propeller

--- inc/PROpeller.h
+++ inc/PROpeller.h
@@ -27,7 +27,7 @@ namespace PROfit{
 		bin_indices.clear();
 	               }
 
-            PROpeller(std::vector<float> intruth, std::vector<float> reco, std::vector<float> baseline, std::vector<int> pdg, std::vector<float> added_weights, std::vector<int> bin_indices) : nevents(truth.size()), truth(intruth){}
+            PROpeller(std::vector<float> intruth, std::vector<float> inreco, std::vector<float> inbaseline, std::vector<int> inpdg, std::vector<float> inadded_weights, std::vector<int> inbin_indices) : nevents(truth.size()), truth(intruth), reco(inreco), baseline(inbaseline), pdg(inpdg), added_weights(inadded_weights), bin_indices(inbin_indices){}
 
            std::vector<float> truth;
            std::vector<float> reco;
