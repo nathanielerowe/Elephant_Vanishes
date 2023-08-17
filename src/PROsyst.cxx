e699dd8f9f237c9c629bb726d0b029d8b7285f0a
Guanqun Ge <gg2690@columbia.edu>
2023-08-17 15:44:52 -0500

    fix a bug in printing

--- src/PROsyst.cxx
+++ src/PROsyst.cxx
@@ -303,7 +303,7 @@ Eigen::MatrixXd PROsyst::GrabMatrix(const std::string& sys) const{
     if(covmat_map.find(sys) != covmat_map.end())
 	return covmat_map.at(sys);	
     else{
-	log<LOG_ERROR>(L"%1% || Systematic you asked for : %2% doesn't have matrix saved yet..") % __func__ % sys;
+	log<LOG_ERROR>(L"%1% || Systematic you asked for : %2% doesn't have matrix saved yet..") % __func__ % sys.c_str();
 	log<LOG_ERROR>(L"%1% || Return empty matrix .") % __func__ ;
     }
     return Eigen::MatrixXd();
