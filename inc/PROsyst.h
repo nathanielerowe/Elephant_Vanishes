c4358a112a29697202f1164e86549bd03ec6973d
Jacob Larkin <larkin.jacob@gmail.com>
2023-08-17 10:45:31 -0500

    Add constructor and members to PROsyst header

--- inc/PROsyst.h
+++ inc/PROsyst.h
@@ -1,10 +1,22 @@
 #ifndef PROSYST_H_
 #define PROSYST_H_
 
+#include "PROcreate.h"
+
+#include <vector>
+
 namespace PROfit {
 
 class PROsyst {
+public:
+    using Spline = std::vector<std::vector<std::array<float, 4>>>;
 
+    PROsyst(const std::vector<SystStruct>& systs);
+
+private:
+    std::map<std::string, Spline> splines;
+    std::map<std::string, Eigen::MatrixXd> covmats;
+    //std::<std:string, MFA> mfa;
 };
 
 };
