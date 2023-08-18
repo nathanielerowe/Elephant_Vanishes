b155103f24bb34d027d81d51409d64bb5dfc2817
markrosslonergan <markrosslonergan@gmail.com>
2023-08-18 14:13:08 -0500

    Some layout in PROchi

--- inc/PROchi.h
+++ inc/PROchi.h
@@ -28,12 +28,24 @@ namespace PROfit{
             PROchi(const std::string tag, const PROconfig *conin, const PROpeller *pin, const PROsyst *systin, const PROsc *oscin) : model_tag(tag), config(conin), peller(pin), syst(systin), osc(oscin) {last_value = 0.0; last_param = Eigen::VectorXd::Zero(config->m_num_bins_total); }
             float operator()(const Eigen::VectorXd &param, Eigen::VectorXd &gradient)
             {
-               
-                Eigen::VectorXd diff = param-last_param;
+              
+                // Get Spectra from FillRecoSpectra
+
+                // Calcuate Full Covariance matrix
+                
+                // Collapse Covariance and Spectra 
+                
+                // Invert Collaped Matrix Matrix 
+
+                // Calculate Chi^2  value
+                //Put actuall chi^2 here :)
                 float value = param[0]+gradient[0];
 
-                //gradient = (last_value-value)/diff;
+                // Simple gradient here
+                Eigen::VectorXd diff = param-last_param;
+                gradient = (value-last_value)/diff.array();
 
+                //Update last param
                 last_param = param;
                 last_value = value;
                 return value;
