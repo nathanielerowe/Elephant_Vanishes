99906f6820fbc8e3470eeb5ff4c121ab30738696
Mark Ross-Lonergan <markrosslonergan@gmail.com>
2024-09-18 12:02:58 -0500

    compiled

--- inc/PROsurf.h
+++ inc/PROsurf.h
@@ -3,13 +3,27 @@
 
 #include "PROconfig.h"
 #include "PROsc.h"
-#include "PROpeller.h"
 #include "PROspec.h"
+#include "PROpeller.h"
 #include "PROsyst.h"
+#include "PROchi.h"
+
+#include "LBFGSB.h"
 
 #include <Eigen/Eigen>
 
+#include <random>
+#include <thread>
+#include <future>
+
+
 namespace PROfit {
+
+struct surfOut{
+    std::vector<int> grid_index;
+    std::vector<float> grid_val;
+    double chi;
+};
 
 class PROsurf {
 public:
@@ -25,6 +39,8 @@ public:
     PROsurf(size_t nbinsx, const Eigen::VectorXd &edges_x, size_t nbinsy, const Eigen::VectorXd &edges_y) : nbinsx(nbinsx), nbinsy(nbinsy), edges_x(edges_x), edges_y(edges_y), surface(nbinsx, nbinsy) { }
 
     PROsurf(size_t nbinsx, LogLin llx, double x_lo, double x_hi, size_t nbinsy, LogLin lly, double y_lo, double y_hi);
+
+    std::vector<surfOut> PointHelper(const PROconfig *config, const PROpeller *prop, const PROsyst *systs, const PROsc *osc, const PROspec *data, std::vector<surfOut> multi_physics_params, PROchi::EvalStrategy strat, bool binned_weighting, int start, int end);
 
     void FillSurfaceSimple(const PROconfig &config, const PROpeller &prop, const PROsyst &systs, const PROsc &osc, const PROspec &data, std::string filename, bool binned_weighting, int nthreads = 1);
     void FillSurface(const PROconfig &config, const PROpeller &prop, const PROsyst &systs, const PROsc &osc, const PROspec &data, std::string filename, bool binned_weighting, int nthreads = 1);
