b5661032a0cb63420dd846d7988f8b792d14c5e1
Gray Putnam <gputnam@fnal.gov>
2024-12-27 12:47:35 -0800

    Make PROmetric destructor virtual to prevent undefined behavior.

--- inc/PROmetric.h
+++ inc/PROmetric.h
@@ -9,6 +9,7 @@ class PROmetric {
 public:
     virtual double operator()(const Eigen::VectorXd &param, Eigen::VectorXd &gradient) = 0;
     virtual double operator()(const Eigen::VectorXd &param, Eigen::VectorXd &gradient, bool nograd) = 0;
+    virtual ~PROmetric() {}
 };
 
 };
